626c53e79a803e4ba9568e1fc16de5ffb58de465
jeong <ukidd12@naver.com>
2023-02-18 21:09:10 +0900

    코드 정리

--- 프로그래머스/Lv.1/개인정보 수집 유효기간.py
+++ 프로그래머스/Lv.1/개인정보 수집 유효기간.py
@@ -1,56 +1,3 @@
-# 첫번째 풀이
-# privacies dict 변환
-def change_privacies(privacies):
-    dic_privacies = {}
-
-    for i in privacies:
-        s = list(i.split())
-        dic_privacies[s[0]] = s[1]
-
-    return dic_privacies
-
-# privacies dict 변환
-def change_terms(terms):
-    dic_terms = {}
-
-    for i in terms:
-        s = list(i.split())
-        dic_terms[s[0]] = s[1]
-
-    return dic_terms
-
-# time operation
-def time_operation(date):
-    time = (int(date[0])*12*28) + (int(date[1])*28) + int(date[2])
-    return time
-
-
-def solution(today, terms, privacies):
-    answer = []
-
-    # dict 변환 privacies ( O(N) )
-    privacies = change_privacies(privacies)
-
-    # dict 변환 terms ( O(N) )
-    terms = change_terms(terms)
-
-    # today list 변환
-    today = list(today.split('.'))
-
-    # 오늘 날짜 시간 계산 ( O(1) )
-    today_total = time_operation(today)
-
-    for i,(k,v) in enumerate((privacies.items())):
-        date = list(k.split('.'))
-        term = int(terms.get(v)) * 28
-        date_total = time_operation(date)
-        term_date = today_total - date_total
-
-        if term <= term_date:
-            answer.append(i+1)
-
-    return answer
-
 # terms를 dictionary로 변환
 def term_dict(terms):
     d_tmp = {}
@@ -101,4 +48,4 @@ print(solution2(i[0], i[1], i[2]))
 # 테스트 케이스 10, 12, 13, 15 ~ 20 실패
 # 예상 오류
 # 1. 현재 privacies에 데이터를 dictionary로 저장 하였느데 같은 날짜에 다른 약관 종류가 들어온다면 인지하지 못한다.
-# 2. 예상 해결 방법 - 이차월 배열로 계산을 할예정
+# 2. 예상 해결 방법 - 이차원 배열로 계산을 할예정 ( 해결 )
