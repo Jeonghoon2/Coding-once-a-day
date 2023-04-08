8bce04721611b8ca8c06d5c727a67c97bd72149d
Jeonghoon2 <ukidd12@naver.com>
2023-04-08 18:09:47 +0900

    로또의 최고 순위와 최저 순위

--- 프로그래머스/Lv.1/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/Lv.1/로또의 최고 순위와 최저 순위.py
@@ -1,22 +1,20 @@
+# high rank 와 low rank 를 구해야한다.
+# 0인 부분은 high rank 일 경우 모두 정답처리
+# Low 인 경우에는 모두 오답 처리
+
 def solution(lottos, win_nums):
-    answer = []
-    rank = [6,5,4,3,2,1]
-    correct = 0
-    guessNum = lottos.count(0)
-    
+    correct_cnt = 0
+    zero_cnt = lottos.count(0)
+
+    rank = [6, 6, 5, 4, 3, 2, 1]
+
     for i in lottos:
-        if i != 0:
-            correct += win_nums.count(i)
-    
-    min = rank.index(correct)
-    max = rank.index(correct+guessNum)
-    
-    
-    answer.append(max+1)
-    answer.append(min+1)
-    
-    
-    return answer
+        if i in win_nums:
+            correct_cnt += 1
+
+    return [rank[zero_cnt+correct_cnt], rank[correct_cnt]]
 
 
-print(solution([45, 4, 35, 20, 3, 9],[20, 9, 3, 45, 4, 35]))
+lottos = [44, 1, 0, 0, 31, 25]
+win_nums = [31, 10, 45, 1, 6, 19]
+print(solution(lottos, win_nums))
