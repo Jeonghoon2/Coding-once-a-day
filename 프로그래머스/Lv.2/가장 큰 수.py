6ee800b8d8aaa3806e6efb2dff85687136dc92d5
Jeonghoon2 <ukidd12@naver.com>
2023-05-07 00:52:35 +0900

    가장 큰 수

--- 프로그래머스/Lv.2/가장 큰 수.py
+++ 프로그래머스/Lv.2/가장 큰 수.py
@@ -1,13 +1,20 @@
 from itertools import permutations
 
 
-def solution(numbers):
-    answer = ''
-    num_list = []
-
-
+# 첫번째 방법
+# 시간 초과
+def solution(num):
+    p_nums = list(permutations(num, len(num)))
+    list_permute = [''.join(map(str, i)) for i in p_nums]
+    answer = max(list_permute)
 
     return answer
 
+# 참조
+def solution2(num):
+    num = list(map(str,num))
+    num.sort(key=lambda x : x*3, reverse=True)
+    return str(int(''.join(num)))
 
-print(solution([6, 10, 2]))
+
+print(solution([6, 20, 10, 2]))
