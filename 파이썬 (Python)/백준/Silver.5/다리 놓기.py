81bab78a2625e16375481fb45223c4e08196e46c
Jeonghoon2 <ukidd12@naver.com>
2023-08-27 19:03:24 +0900

    다리 놓기

--- 파이썬 (Python)/백준/Silver.5/다리 놓기.py
+++ 파이썬 (Python)/백준/Silver.5/다리 놓기.py
@@ -1,17 +1,13 @@
+def factorial(n):
+    num = 1
+    for i in range(1, n + 1):
+        num *= i
+    return num
+
 for _ in range(int(input())):
+
     N, M = map(int, input().split())
 
-    dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
+    bridge = factorial(M) // (factorial(N) * factorial(M-N))
 
-    for i in range(1, N + 1):
-        for j in range(1, M +1):
-            if i == 1:
-                dp[i][j] = j
-                continue
-            elif i == j:
-                dp[i][j] = 1
-            else:
-                if i > j:
-                    dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
-
-    print(dp[N][M])
+    print(bridge)
