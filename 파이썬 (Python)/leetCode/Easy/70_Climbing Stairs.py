60665f9d9325d08512b4e6ace98b9391cccdb8c9
Jeonghoon2 <ukidd12@naver.com>
2023-09-01 01:51:12 +0900

    Climbing Stairs

--- 파이썬 (Python)/leetCode/Easy/70_Climbing Stairs.py
+++ 파이썬 (Python)/leetCode/Easy/70_Climbing Stairs.py
@@ -1,14 +1,20 @@
-# Each time you can either climb 1 or 2 steps.
+# F(1) = 1, F(2) = 2
+# Bottom up
 
 class Solution:
-    def climbStairs(n: int) -> int:
-        if n <= 2:
-            return n
-        a, b = 1, 2
-        for _ in range(3, n+1):
-            tmp = a+b
-            a=b
-            b=tmp
-        return b
+    def climbStairs(n):
+        dp = [0] * (n + 1)
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            dp[0], dp[1] = 1, 2
 
-print(Solution.climbStairs(5))
+            for i in range(2, n + 1):
+                dp[i] = dp[i - 1] + dp[i - 2]
+
+            return dp[n-1]
+
+
+print(Solution.climbStairs(3))
