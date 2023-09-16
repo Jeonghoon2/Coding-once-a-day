d387151413a33885890929be2f772f6b3eb7c818
Jeonghoon2 <ukidd12@naver.com>
2023-09-16 18:41:31 +0900

    N으로 표현

--- 파이썬 (Python)/프로그래머스/Lv.3/N으로 표현.py
+++ 파이썬 (Python)/프로그래머스/Lv.3/N으로 표현.py
@@ -1,26 +1,26 @@
 # 1<= N <= 9
 
-
-def solution(N, number):
+def solution2(N, number):
     if number == 1:
         return 1
+
     set_list = []
 
-    for cnt in range(1, 9):  # 1개부터 8개까지 확인
-        partial_set = set()
-        partial_set.add(int(str(N) * cnt))  # 이어 붙여서 만드는 경우 넣기
-        for i in range(cnt - 1):  # (1, n-1) 부터 (n-1, 1)까지 사칙연산
+    for cnt in range(1, 9):
+        tmp_set = set()
+        tmp_set.add(int(str(N) * cnt))
+
+        for i in range(cnt - 1):
             for op1 in set_list[i]:
                 for op2 in set_list[-i - 1]:
-                    partial_set.add(op1 + op2)
-                    partial_set.add(op1 * op2)
-                    partial_set.add(op1 - op2)
+                    tmp_set.add(op1 + op2)
+                    tmp_set.add(op1 * op2)
+                    tmp_set.add(op1 - op2)
                     if op2 != 0:
-                        partial_set.add(op1 / op2)
-        # 만든 집합에 number가 처음 나오는지 확인
-        if number in partial_set:
+                        tmp_set.add(op1 / op2)
+        if number in tmp_set:
             return cnt
-        set_list.append(partial_set)
+        set_list.append(tmp_set)
     return -1
 
 
