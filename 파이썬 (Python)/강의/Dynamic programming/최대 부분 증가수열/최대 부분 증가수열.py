5b747c3c998ce95240deda8c41e11383e976996d
Jeonghoon2 <ukidd12@naver.com>
2023-11-16 18:06:18 +0900

    코드 간결화

--- 파이썬 (Python)/강의/Dynamic programming/최대 부분 증가수열/최대 부분 증가수열.py
+++ 파이썬 (Python)/강의/Dynamic programming/최대 부분 증가수열/최대 부분 증가수열.py
@@ -11,17 +11,10 @@ if __name__ == '__main__':
     dy[0] = 1
 
     for i in range(1, n):
-        sub = i - 1
-        while True:
-
-            if sub < 0:
-                break
+        for j in range(0, i):
+            if arr[i] > arr[j]:
+                dy[i] = max(dy[j] + 1, dy[i])
             else:
-                if arr[i] > arr[sub]:
-                    dy[i] = max(dy[i], dy[sub] + 1)
-                    sub -= 1
-                else:
-                    dy[i] = max(dy[i], 1)
-                    sub -= 1
+                dy[i] = max(dy[i], 1)
 
     print(max(dy))
