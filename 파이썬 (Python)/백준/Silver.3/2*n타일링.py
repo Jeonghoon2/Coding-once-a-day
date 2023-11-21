e6a677f7c2b3bdaf0778c758ea49a43fd581d9b1
Jeonghoon2 <ukidd12@naver.com>
2023-11-21 17:57:27 +0900

    계단 오르기, 2xn타일링 (시간초과 해결)

--- 파이썬 (Python)/백준/Silver.3/2*n타일링.py
+++ 파이썬 (Python)/백준/Silver.3/2*n타일링.py
@@ -1,14 +1,15 @@
-def solution(n):
+import sys
 
-    if n == 1:
-        return 1
+n = int(input())
 
-    dp = [0 for _ in range(n)]
-    dp[0], dp[1] = 1, 2
+if n == 1:
+    print(1)
+    sys.exit(0)
 
-    for i in range(2, n):
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+dp = [0] * (n + 1)
+dp[1], dp[2] = 1, 3
 
-    return dp[n - 1]
+for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + (dp[i - 2] * 2)
 
-print(solution(int(input())))
+print(dp[n] % 10007)
--- 파이썬 (Python)/백준/Silver.3/계단 오르기.py
+++ 파이썬 (Python)/백준/Silver.3/계단 오르기.py
@@ -1,54 +1,11 @@
-def solution():
-    n = int(input())
-    stairs = []
-    visit = [0] * n
-    for _ in range(n):
-        stairs.append(int(input()))
+n = int(input())
 
-    def dfs(idx, cnt, dp, v):
-        move = [1, 2]
+arr = [0] + [int(input()) for _ in range(n)]
 
-        for m in move:
-            c_v = v.copy()
-            c_dp = dp.copy()
-            n_idx = idx + m
+dp = [[0] * 3 for _ in range(n + 1)]
+for i in range(1, n + 1):
+    dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
+    dp[i][1] = dp[i - 1][0] + arr[i]
+    dp[i][2] = dp[i - 1][1] + arr[i]
 
-            if n_idx >= len(dp):
-                return
-
-            if idx >= 1 and c_dp[n_idx - 1] > 0 and c_dp[n_idx - 2] > 0 and m == 1:
-                continue
-            else:
-                c_v.append(stairs[n_idx])
-                c_dp[n_idx] = sum(c_v)
-            if n_idx < len(stairs):
-                if visit[n_idx] < c_dp[n_idx]:
-                    visit[n_idx] = c_dp[n_idx]
-                dfs(n_idx, cnt + 1, c_dp, c_v)
-            else:
-                return
-
-    dfs(-1, 0, [0] * n, [])
-
-    return visit[-1]
-
-
-# 인터넷 참조
-def solution2():
-    # input
-    n = int(input())
-    stairs = [int(input()) for _ in range(n)]
-    dp = [0] * n
-
-    if len(stairs) <= 2:
-        print(sum(stairs))
-    else:
-        dp[0] = stairs[0]
-        dp[1] = stairs[0] + stairs[1]
-
-        for i in range(2,n):
-            dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2]+stairs[i])
-
-    print(dp[-1])
-
-print(solution2())
+print(max(dp[n][1], dp[n][2]))
