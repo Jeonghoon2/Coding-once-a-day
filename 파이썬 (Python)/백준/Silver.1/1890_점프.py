6bae7450cf42234b2e5918e585cd39a0377d4a62
Jeonghoon2 <ukidd12@naver.com>
2023-12-03 20:15:01 +0900

    점프

--- 파이썬 (Python)/백준/Silver.1/1890_점프.py
+++ 파이썬 (Python)/백준/Silver.1/1890_점프.py
@@ -1,15 +1,25 @@
-# N x N 게임판
-# 오른쪽이나 아래쪽으로만 이동해야 한다.
+# 경로의 개수를 구한다.
+# 규칙 : 오른쪽, 아래 만 이동 가능
 
-def solution(n, l):
-    answer = 0
+n = int(input())
+lst = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = 1
 
+for i in range(n):
+    for j in range(n):
 
-    return answer
+        # 도착 지점 도달 했을 경우
+        if i == n - 1 and j == n - 1:
+            print(dp[i][j])
+            break
 
+        # 우측 이동
+        # 범위를 벗어 나지 않을 경우
+        if j + lst[i][j] < n:
+            dp[i][j + lst[i][j]] += dp[i][j]
 
-n = 4
-l = []
-for _ in range(n):
-    l.append(list(input().split()))
-print(solution(n, l))
+        # 아래 이동
+        # 범위를 벗어 나지 않을 경우
+        if i + lst[i][j] < n:
+            dp[i + lst[i][j]][j] += dp[i][j]
